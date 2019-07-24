f73c08b0d6b0fc3f01d37ab4e0986c7e044e0504
Herok4Build <thomash.johnson261@gmail.com>
2019-07-24 11:59:08 -0600

    Changed the file name of the Fortran file.

--- casper_fortran_script_for_execution.sh
+++ casper_fortran_script_for_execution.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -l
 ## Authors: Thomas Johnson III and Brian Vanderwende
 #SBATCH --job-name=fortrancasperjobsubmission
-#SBATCH --account=SCSG0002
+#SBATCH --<account>
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=00:17:00
 #SBATCH --partition=dav
 #SBATCH -o fortran_casper_job_output.%j
 
-export TMPDIR=/glade/scratch/thomasjo/temp
+export TMPDIR=/glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-ifort -o execution_code_fortran contstructMultiDimensionalItem.f90
+ifort -o execution_code_fortran constructMultiDimensionalItem.f90
 export OMP_NUM_THREADS=1
-srun ./execution_code_fortran_pile 350 350 350
+srun ./execution_code_fortran_pile 80 80 80
--- cheyenne_fortran_script_for_execution.sh
+++ cheyenne_fortran_script_for_execution.sh
@@ -2,16 +2,15 @@
 ## Authors: Thomas Johnson III and Brian Vanderwende
 ## Script that displays output of Peak Memory Usage for the Fortran Script on Cheyenne
 #PBS -N fortran_cheyenne_peakmemusage
-#PBS -A SCSG0002
+#PBS -A <account>
 #PBS -l walltime=00:17:00
 #PBS -q regular
 #PBS -j oe
 #PBS -m abe
-#PBS -M thomasjo@ucar.edu
 #PBS -l select=1:ncpus=1
 
-setenv TMPDIR /glade/scratch/thomasjo/temp
+setenv TMPDIR /glade/scratch/$USER/temp
 mkdir -p $TMPDIR
 
-ifort -o execution_code_fortran contstructMultiDimensionalItem.f90
+ifort -o execution_code_fortran constructMultiDimensionalItem.f90
 ./execution_code_fortran
